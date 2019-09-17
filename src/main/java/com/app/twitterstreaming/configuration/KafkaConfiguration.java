bc1f0e348bd875932498b8b34e68982c3aa22167
Nitesh Kumar Giri <giri17nit@gmail.com>
2019-09-17 15:41:14 +0530

    Modified configuration file for Twitter Configurtion.

--- src/main/java/com/app/twitterstreaming/configuration/KafkaConfiguration.java
+++ src/main/java/com/app/twitterstreaming/configuration/KafkaConfiguration.java
@@ -1,6 +1,9 @@
 package com.app.twitterstreaming.configuration;
 
 public class KafkaConfiguration {
+	
+	private KafkaConfiguration() {}
+	
     public static final String SERVERS = "localhost:9092";
     public static final String TOPIC = "TheSkyIsPink";
     public static final long SLEEP_TIMER = 1000;
